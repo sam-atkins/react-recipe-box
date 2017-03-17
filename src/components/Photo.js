858550898a636744a0a2cd701120fb3cf73d7308
Sam Atkins <sam@cubi.io>
2017-03-17 12:50:54 +0000

    feat: Displays single recipe

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -3,16 +3,21 @@ import { Link } from 'react-router';
 
 class Photo extends React.Component {
   render() {
+    
+    const { recipe } = this.props;
+    // deconstructed above, is the same as the below
+    // const recipe = this.props.recipe;
+    
     return (
       <figure className="grid-figure">
         <div className="grip-photo-wrap">
-          <Link to={`/view/${this.props.recipe.meal_code}`}>
-            {this.props.recipe.meal}
-            <img src={this.props.recipe.display_src} alt={this.props.recipe.meal} className="grid-photo"/>
+          <Link to={`/view/${recipe.meal_code}`}>
+            <img src={recipe.display_src} alt={recipe.meal} className="grid-photo"/>
           </Link>
+          <span>{recipe.meal}</span>
         </div>
         <figcaption>
-          <p>{this.props.recipe.description}</p>
+          <p>{recipe.description}</p>
         </figcaption>
       </figure>
     )
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 class Recipe extends Component {
   render() {
+    // find the index of the recipe
+    const i = this.props.recipes.findIndex((recipe) => recipe.meal_code === this.props.params.postId);
+    // console.log(i);
+
+    // get the recipe in order to render it
+    const recipe = this.props.recipes[i];
+    // console.log(recipe);
+
     return (
-      <div className="default">I'm a single recipe</div>
+      <div className="single-photo">
+        <Photo i={i} recipe={recipe} {...this.props} />
+      </div>
     )
   }
 }
