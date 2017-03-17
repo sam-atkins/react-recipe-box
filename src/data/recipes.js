20340726b0b0c37f5a5b6cfb2d5886d8235f2dea
Sam Atkins <sam@cubi.io>
2017-03-17 15:46:59 +0000

    refactor: Amend data layout for ingredients

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -2,16 +2,20 @@ const recipes = [
    {
       "meal_code":"tcy001",
       "meal":"Thai curry",
-      "description":"Beautify, yummy Thai curry",
+      "description":"Beautiful, yummy Thai curry",
       "likes":43,
-      "display_src":"http://i65.tinypic.com/24vnvxc.jpg"
+      "display_src":"http://i65.tinypic.com/24vnvxc.jpg",
+      "ingredients" : 
+         ["Prawns", "Coconut milk", "Lemongrass", "Red chillis"]
    },
    {
       "meal_code":"csp001",
       "meal":"Chicken Soup",
       "description":"Feeling ill? This soup is just what the doctor ordered!",
       "likes":10,
-      "display_src":"http://i64.tinypic.com/dekxgo.jpg"
+      "display_src":"http://i64.tinypic.com/dekxgo.jpg",
+      "ingredients" : 
+         ["Chicken", "Carrot", "Chicken stock", "Sweetcorn"]
    }
 ];
 
