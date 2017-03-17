ea44e458dde3d3da4b2997edfef68857affebbd3
Sam Atkins <sam@cubi.io>
2017-03-17 15:46:39 +0000

    feat: Recipe ingredients rendered on Recipe page

---
+++ src/components/Ingredients.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Ingredients extends React.Component {
+  constructor() {
+    super();
+
+    // bind helpers i.e. allows use of 'this' for custom methods
+    this.renderIngredients = this.renderIngredients.bind(this);
+
+  }
+
+  renderIngredients(ingredient, i) {
+  
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{ingredient}</strong>
+        </p>
+      </div>
+    )
+  }
+ 
+  render() {
+    // console.log(this);
+    
+    // deconstructor to const to help keep code cleaner
+    const { recipes } = this.props;
+
+    // find the index of the recipe
+    const i = recipes.findIndex((recipe) => recipe.meal_code === this.props.params.postId);
+    console.log(`This is the recipe index = ${i}`);
+    
+    console.log(`These are the ingredients: ${recipes[i].ingredients}`);
+    return (
+      <div className="comments">
+        {recipes[i].ingredients.map(this.renderIngredients)}
+      </div>
+    )
+  }
+}
+
+export default Ingredients;
