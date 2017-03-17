97db7c70dfee332eb749426cd8383d05e5151731
Sam Atkins <sam@cubi.io>
2017-03-17 15:49:23 +0000

    feat: set-up Ingredients child component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Photo from './Photo';
+import Ingredients from './Ingredients';
 
 class Recipe extends Component {
   render() {
+
     // find the index of the recipe
     const i = this.props.recipes.findIndex((recipe) => recipe.meal_code === this.props.params.postId);
-    // console.log(i);
+    // console.log(`This is the recipe index = ${i}`);
 
     // get the recipe in order to render it
     const recipe = this.props.recipes[i];
@@ -14,6 +16,7 @@ class Recipe extends Component {
     return (
       <div className="single-photo">
         <Photo i={i} recipe={recipe} {...this.props} />
+        <Ingredients {...this.props}/>
       </div>
     )
   }
